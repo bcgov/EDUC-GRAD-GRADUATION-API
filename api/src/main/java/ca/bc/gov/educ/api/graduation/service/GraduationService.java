1e51c44e9dae6ee90c299341326d14ec4715dc3f
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2020-12-14 01:36:50 -0800

    KeyCloak Changes

--- api/src/main/java/ca/bc/gov/educ/api/graduation/service/GraduationService.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/service/GraduationService.java
@@ -1,22 +1,17 @@
 package ca.bc.gov.educ.api.graduation.service;
 
 
-import java.util.HashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import ca.bc.gov.educ.api.graduation.model.dto.GraduationStatus;
-import ca.bc.gov.educ.api.graduation.model.dto.Student;
 import ca.bc.gov.educ.api.graduation.util.EducGraduationApiConstants;
 import ca.bc.gov.educ.api.graduation.util.EducGraduationApiUtils;
 
@@ -35,66 +30,16 @@ public class GraduationService {
     private String updateGradStatusForStudent;    
 
     
-	public GraduationStatus graduateStudentByPen(String pen) {
+	public GraduationStatus graduateStudentByPen(String pen, String accessToken) {
 		logger.info("graduateStudentByPen");
-		GraduationStatus graduationStatus = restTemplate.getForObject(String.format(graduateStudent,pen), GraduationStatus.class);
-		ResponseEntity<GraduationStatus> response = restTemplate.postForEntity(String.format(updateGradStatusForStudent,pen), graduationStatus, GraduationStatus.class);
-		
-		//create reportparameter and call report api.
-		ObjectMapper mapper = new ObjectMapper();
-		Student student = new Student();
-		if(response.getBody().getStudentGradData() != null) {
-			try {
-				student = mapper.readValue(response.getBody().getStudentGradData().toString(), Student.class);
-			} catch (JsonMappingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}		
-			
-			HashMap<String, String> reportParameters = new HashMap<String, String>();
-	        reportParameters.put("UPDATE_DATE",EducGraduationApiUtils.formatDate(EducGraduationApiConstants.DEFAULT_UPDATED_TIMESTAMP));
-	        reportParameters.put("NAME", student.getLastName() + ", " + student.getFirstName() + " "
-	                                + student.getMiddleName());
-	        reportParameters.put("PEN", student.getPen());
-	        reportParameters.put("GRAD_PROGRAM", student.getGraduationProgram());
-	        reportParameters.put("SCHOOL_NAME", student.getSchool());
-	        reportParameters.put("DOB", student.getDateOfBirth());
-	        reportParameters.put("GRADE", student.getGrade());
-	        reportParameters.put("GENDER", student.getGender());
-	        reportParameters.put("ADDRESS1", student.getAddress().getAddress1());
-	        reportParameters.put("ADDRESS2", student.getAddress().getAddress2());
-	        reportParameters.put("CITY", student.getAddress().getCity());
-	        reportParameters.put("POSTAL", student.getAddress().getPostalCode());
-	        reportParameters.put("PROVINCENAME", student.getAddress().getProvinceName());
-	        reportParameters.put("COUNTYNAME", student.getAddress().getContryName());
-	        reportParameters.put("ADDRESS1", student.getAddress().getAddress1());
-	        reportParameters.put("GRADUATION_MESSAGE", student.getGraduationMessage());
-	
-	        StringBuffer tableData = new StringBuffer();        
-	        reportParameters.put("TABLE_DATA", tableData.toString());
-	        
-	        StringBuffer reqMetList = new StringBuffer("<ul>");
-	        for (String s : student.getRequirementsMet()) {
-	            reqMetList.append("<li>" +s + "</li>");
-	        }
-	        reqMetList.append("</ul>");
-	        reportParameters.put("REQ_MET_LIST", reqMetList.toString());
-	
-	        StringBuffer reqNotMetList = new StringBuffer("<ul>");
-	        for (String s : student.getRequirementsNotMet()) {
-	            reqNotMetList.append("<li>" +s + "</li>");
-	        }
-	        reqMetList.append("</ul>");
-	        reportParameters.put("REQ_NOT_MET_LIST", reqNotMetList.toString());
-		}
-		//Save generated reports in Student Report Table
-		
-		
-		return response.getBody();
-	    
+		HttpHeaders httpHeaders = EducGraduationApiUtils.getHeaders(accessToken);
+		GraduationStatus graduationStatus = restTemplate.exchange(String.format(graduateStudent,pen), HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), GraduationStatus.class).getBody();
+		GraduationStatus graduationStatusResponse = restTemplate.exchange(String.format(updateGradStatusForStudent,pen), HttpMethod.POST,
+				new HttpEntity<>(graduationStatus,httpHeaders), GraduationStatus.class).getBody();
+		//create reportparameter and call report api.		
+		//Save generated reports in Student Report Table		
+		return graduationStatusResponse;	    
 	}
 
     
---
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/util/PermissionsContants.java
@@ -0,0 +1,8 @@
+package ca.bc.gov.educ.api.graduation.util;
+
+public interface PermissionsContants {
+	String _PREFIX = "#oauth2.hasAnyScope('";
+	String _SUFFIX = "')";
+
+	String GRADUATE_STUDENT = _PREFIX + "UPDATE_GRAD_GRADUATION_STATUS', 'RUN_GRAD_ALGORITHM" + _SUFFIX;
+}
