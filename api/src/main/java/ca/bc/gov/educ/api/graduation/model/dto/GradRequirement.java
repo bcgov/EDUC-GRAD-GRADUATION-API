b9f6046ef366d405f63c6c251fc0a08f6775c548
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-02-02 14:15:56 -0800

    bug fix

---
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/model/dto/GradRequirement.java
@@ -0,0 +1,15 @@
+package ca.bc.gov.educ.api.graduation.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GradRequirement {
+    String rule;
+    String description;
+}
