480cda2e860b563b7fddfd0497b57daf917f57d7
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-20 17:24:13 -0700

    Change to webclient

--- api/src/main/java/ca/bc/gov/educ/api/graduation/service/GraduationService.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/service/GraduationService.java
@@ -12,11 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -40,6 +38,7 @@ import ca.bc.gov.educ.api.graduation.model.dto.StudentDemographics;
 import ca.bc.gov.educ.api.graduation.util.EducGraduationApiConstants;
 import ca.bc.gov.educ.api.graduation.util.EducGraduationApiUtils;
 import ca.bc.gov.educ.api.graduation.util.GradBusinessRuleException;
+import reactor.core.publisher.Mono;
 
 
 @Service
@@ -47,7 +46,10 @@ public class GraduationService {
 
 	private static Logger logger = LoggerFactory.getLogger(GraduationService.class);
 
-    @Autowired
+	@Autowired
+    WebClient webClient;
+	
+	@Autowired
     RestTemplate restTemplate;
 
     @Value(EducGraduationApiConstants.ENDPOINT_GRADUATION_ALGORITHM_URL)
@@ -92,29 +94,24 @@ public class GraduationService {
 	public AlgorithmResponse graduateStudentByPen(String pen, String accessToken) {
 		logger.debug("graduateStudentByPen");
 		AlgorithmResponse algorithmResponse = new AlgorithmResponse();
-		HttpHeaders httpHeaders = EducGraduationApiUtils.getHeaders(accessToken);
 		List<CodeDTO> specialProgram = new ArrayList<>();
 		try {
 
-		GraduationStatus gradResponse = restTemplate.exchange(String.format(updateGradStatusForStudent,pen), HttpMethod.GET,
-					new HttpEntity<>(httpHeaders), GraduationStatus.class).getBody();
+		GraduationStatus gradResponse = webClient.get().uri(String.format(updateGradStatusForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GraduationStatus.class).block();
 		//Run Grad Algorithm
-		GraduationData graduationDataStatus = restTemplate.exchange(String.format(graduateStudent,pen,gradResponse.getProgram()), HttpMethod.GET,
-				new HttpEntity<>(httpHeaders), GraduationData.class).getBody();
+		GraduationData graduationDataStatus = webClient.get().uri(String.format(graduateStudent,pen,gradResponse.getProgram())).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GraduationData.class).block();
+		 
 		List<GradStudentSpecialProgram> projectedSpecialGradResponse = new ArrayList<GradStudentSpecialProgram>();
 		//Run Special Program Algorithm
 		for(int i=0; i<graduationDataStatus.getSpecialGradStatus().size();i++) {
 			CodeDTO specialProgramCode = new CodeDTO();
 			SpecialGradAlgorithmGraduationStatus specialPrograms = graduationDataStatus.getSpecialGradStatus().get(i);
-			GradStudentSpecialProgram gradSpecialProgram = restTemplate.exchange(String.format(specialProgramDetails,pen,specialPrograms.getSpecialProgramID()), HttpMethod.GET,
-					new HttpEntity<>(httpHeaders), GradStudentSpecialProgram.class).getBody();
-			
+			GradStudentSpecialProgram gradSpecialProgram = webClient.get().uri(String.format(specialProgramDetails,pen,specialPrograms.getSpecialProgramID())).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GradStudentSpecialProgram.class).block();
 			gradSpecialProgram.setSpecialProgramCompletionDate(specialPrograms.getSpecialProgramCompletionDate());
 			gradSpecialProgram.setStudentSpecialProgramData(new ObjectMapper().writeValueAsString(specialPrograms));
 			
-			restTemplate.exchange(saveSpecialGradStatusForStudent, HttpMethod.POST,
-						new HttpEntity<>(gradSpecialProgram,httpHeaders), GradStudentSpecialProgram.class).getBody();
-			
+			//Save Special Grad Status
+			webClient.post().uri(saveSpecialGradStatusForStudent).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(gradSpecialProgram), GradStudentSpecialProgram.class).retrieve().bodyToMono(GradStudentSpecialProgram.class);
 			specialProgramCode.setCode(gradSpecialProgram.getSpecialProgramCode());
 			specialProgramCode.setName(gradSpecialProgram.getSpecialProgramName());
 			specialProgram.add(specialProgramCode);
@@ -122,11 +119,9 @@ public class GraduationService {
 		}
 
 		GraduationStatus toBeSaved = prepareGraduationStatusObj(graduationDataStatus);
-		ReportData data = prepareReportData(graduationDataStatus,httpHeaders,specialProgram);
+		ReportData data = prepareReportData(graduationDataStatus,accessToken,specialProgram);
 		if(toBeSaved != null && toBeSaved.getPen() != null) {
-			GraduationStatus graduationStatusResponse = restTemplate.exchange(String.format(updateGradStatusForStudent,pen), HttpMethod.POST,
-					new HttpEntity<>(toBeSaved,httpHeaders), GraduationStatus.class).getBody();
-
+			GraduationStatus graduationStatusResponse = webClient.post().uri(String.format(updateGradStatusForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(toBeSaved), GraduationStatus.class).retrieve().bodyToMono(GraduationStatus.class).block();
 			//Reports
 			data.getDemographics().setMincode(graduationStatusResponse.getSchoolOfRecord());
 			data.setIssueDate(EducGraduationApiUtils.formatDateForReport(graduationStatusResponse.getUpdatedTimestamp().toString()));
@@ -158,7 +153,7 @@ public class GraduationService {
 				}
 				
 				for(String certType : certificateList) {
-					saveStudentCertificateReport(pen,data,httpHeaders,certType,graduationStatusResponse.getStudentID());
+					saveStudentCertificateReport(pen,data,accessToken,certType,graduationStatusResponse.getStudentID());
 				}
 				
 				
@@ -166,8 +161,7 @@ public class GraduationService {
 			List<CodeDTO> certificateProgram = new ArrayList<>();
 			for(String certType : certificateList) {
 				CodeDTO cDTO = new CodeDTO();
-				GradCertificateTypes gradCertificateTypes = restTemplate.exchange(String.format(getGradCertificateType,certType), HttpMethod.GET,
-	    				new HttpEntity<>(httpHeaders), GradCertificateTypes.class).getBody();
+				GradCertificateTypes gradCertificateTypes = webClient.get().uri(String.format(getGradCertificateType,certType)).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GradCertificateTypes.class).block();
 	    		if(gradCertificateTypes != null) {
 	    			cDTO.setCode(gradCertificateTypes.getCode());
 	    			cDTO.setName(gradCertificateTypes.getDescription());
@@ -176,8 +170,8 @@ public class GraduationService {
 			}
 			data.getGraduationMessages().setCertificateProgram(certificateProgram);
 			data.getGraduationMessages().setHasCareerProgram(certificateProgram.size() > 0 ? true:false);
-			saveStudentAchievementReport(pen,data,httpHeaders,graduationStatusResponse.getStudentID());
-			saveStudentTranscriptReport(pen,data,httpHeaders,graduationStatusResponse.getStudentID());			
+			saveStudentAchievementReport(pen,data,accessToken,graduationStatusResponse.getStudentID());
+			saveStudentTranscriptReport(pen,data,accessToken,graduationStatusResponse.getStudentID());			
 
 			algorithmResponse.setGraduationStatus(graduationStatusResponse);
 			algorithmResponse.setSpecialGraduationStatus(projectedSpecialGradResponse);
@@ -193,18 +187,13 @@ public class GraduationService {
 
 		logger.debug("projectStudentGraduationByPen");
 		AlgorithmResponse algorithmResponse = new AlgorithmResponse();
-		HttpHeaders httpHeaders = EducGraduationApiUtils.getHeaders(accessToken);
-
 		try {
 
-			GraduationStatus gradResponse = restTemplate.exchange(String.format(updateGradStatusForStudent,pen), HttpMethod.GET,
-					new HttpEntity<>(httpHeaders), GraduationStatus.class).getBody();
+			GraduationStatus gradResponse = webClient.get().uri(String.format(updateGradStatusForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GraduationStatus.class).block();
 
 			//Run Grad Algorithm
-			GraduationData graduationDataStatus = restTemplate.exchange(String.format(projectedStudentGraduation, pen,
-					gradResponse.getProgram(), true), HttpMethod.GET,
-					new HttpEntity<>(httpHeaders), GraduationData.class).getBody();
-
+			GraduationData graduationDataStatus = webClient.get().uri(String.format(projectedStudentGraduation, pen,gradResponse.getProgram(), true)).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GraduationData.class).block();
+			
 			gradResponse.setStudentGradData(new ObjectMapper().writeValueAsString(graduationDataStatus));
 			gradResponse.setPen(graduationDataStatus.getGradStatus().getPen());
 			gradResponse.setProgram(graduationDataStatus.getGradStatus().getProgram());
@@ -220,8 +209,7 @@ public class GraduationService {
 			for(int i=0; i<graduationDataStatus.getSpecialGradStatus().size();i++) {
 				GradStudentSpecialProgram specialProgramProjectedObj = new GradStudentSpecialProgram();
 				SpecialGradAlgorithmGraduationStatus specialPrograms = graduationDataStatus.getSpecialGradStatus().get(i);
-				GradStudentSpecialProgram gradSpecialProgram = restTemplate.exchange(String.format(specialProgramDetails,pen,specialPrograms.getSpecialProgramID()), HttpMethod.GET,
-						new HttpEntity<>(httpHeaders), GradStudentSpecialProgram.class).getBody();
+				GradStudentSpecialProgram gradSpecialProgram = webClient.get().uri(String.format(specialProgramDetails,pen,specialPrograms.getSpecialProgramID())).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GradStudentSpecialProgram.class).block();
 				specialProgramProjectedObj.setSpecialProgramCompletionDate(specialPrograms.getSpecialProgramCompletionDate());
 				specialProgramProjectedObj.setStudentSpecialProgramData(new ObjectMapper().writeValueAsString(specialPrograms));
 				specialProgramProjectedObj.setPen(pen);
@@ -238,65 +226,59 @@ public class GraduationService {
 		}
 	}
 
-	public void saveStudentAchievementReport(String pen, ReportData data, HttpHeaders httpHeaders, UUID studentID) {
-		String encodedPdfReportAchievement = generateStudentAchievementReport(data,httpHeaders);
+	public void saveStudentAchievementReport(String pen, ReportData data, String accessToken, UUID studentID) {
+		String encodedPdfReportAchievement = generateStudentAchievementReport(data,accessToken);
 		GradStudentReports requestObj = new GradStudentReports();
 		requestObj.setPen(pen);
 		requestObj.setStudentID(studentID);
 		requestObj.setReport(encodedPdfReportAchievement);
 		requestObj.setGradReportTypeCode("ACHV");
-		restTemplate.exchange(String.format(updateGradStudentReportForStudent,pen), HttpMethod.POST,
-						new HttpEntity<>(requestObj,httpHeaders), GradStudentReports.class).getBody();
+		webClient.post().uri(String.format(updateGradStudentReportForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(requestObj), GradStudentReports.class).retrieve().bodyToMono(GradStudentReports.class).block();
 	}
 	
-	public void saveStudentCertificateReport(String pen, ReportData data, HttpHeaders httpHeaders,String certificateType, UUID studentID) {
-		String encodedPdfReportCertificate = generateStudentCertificateReport(data,httpHeaders);
+	public void saveStudentCertificateReport(String pen, ReportData data, String accessToken,String certificateType, UUID studentID) {
+		String encodedPdfReportCertificate = generateStudentCertificateReport(data,accessToken);
 		GradStudentCertificates requestObj = new GradStudentCertificates();
 		requestObj.setPen(pen);
 		requestObj.setStudentID(studentID);
 		requestObj.setCertificate(encodedPdfReportCertificate);
 		requestObj.setGradCertificateTypeCode(certificateType);
-		restTemplate.exchange(String.format(updateGradStudentCertificateForStudent,pen), HttpMethod.POST,
-						new HttpEntity<>(requestObj,httpHeaders), GradStudentCertificates.class).getBody();
+		webClient.post().uri(String.format(updateGradStudentCertificateForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(requestObj), GradStudentCertificates.class).retrieve().bodyToMono(GradStudentCertificates.class).block();
 	}
 
-	public void saveStudentTranscriptReport(String pen, ReportData data, HttpHeaders httpHeaders,UUID studentID) {
-		String encodedPdfReportTranscript = generateStudentTranscriptReport(data,httpHeaders);
+	public void saveStudentTranscriptReport(String pen, ReportData data, String accessToken,UUID studentID) {
+		String encodedPdfReportTranscript = generateStudentTranscriptReport(data,accessToken);
 		GradStudentReports requestObj = new GradStudentReports();
 		requestObj.setPen(pen);
 		requestObj.setReport(encodedPdfReportTranscript);
 		requestObj.setStudentID(studentID);
 		requestObj.setGradReportTypeCode("TRAN");
-		restTemplate.exchange(String.format(updateGradStudentReportForStudent,pen), HttpMethod.POST,
-						new HttpEntity<>(requestObj,httpHeaders), GradStudentReports.class).getBody();
+		webClient.post().uri(String.format(updateGradStudentReportForStudent,pen)).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(requestObj), GradStudentReports.class).retrieve().bodyToMono(GradStudentReports.class).block();
 	}
 
-	private String generateStudentTranscriptReport(ReportData data, HttpHeaders httpHeaders) {
+	private String generateStudentTranscriptReport(ReportData data, String accessToken) {
 		GenerateReport reportParams = new GenerateReport();
 		reportParams.setData(data);
-		byte[] bytesSAR = restTemplate.exchange(reportTranscriptURL, HttpMethod.POST,
-				new HttpEntity<>(reportParams,httpHeaders), byte[].class).getBody();
+		byte[] bytesSAR = webClient.post().uri(reportTranscriptURL).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(reportParams), GenerateReport.class).retrieve().bodyToMono(byte[].class).block();
 		byte[] encoded = Base64.encodeBase64(bytesSAR);
 	    return new String(encoded,StandardCharsets.US_ASCII);
 	}
 
 
 
-	private String generateStudentAchievementReport(ReportData data, HttpHeaders httpHeaders) {
+	private String generateStudentAchievementReport(ReportData data, String accessToken) {
 		GenerateReport reportParams = new GenerateReport();
 		reportParams.setData(data);
-		byte[] bytesSAR = restTemplate.exchange(reportAchievementURL, HttpMethod.POST,
-				new HttpEntity<>(reportParams,httpHeaders), byte[].class).getBody();
+		byte[] bytesSAR = webClient.post().uri(reportAchievementURL).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(reportParams), GenerateReport.class).retrieve().bodyToMono(byte[].class).block();
 		byte[] encoded = Base64.encodeBase64(bytesSAR);
 	    return new String(encoded,StandardCharsets.US_ASCII);
 
 	}
 
-	private String generateStudentCertificateReport(ReportData data, HttpHeaders httpHeaders) {
+	private String generateStudentCertificateReport(ReportData data, String accessToken) {
 		GenerateReport reportParams = new GenerateReport();
 		reportParams.setData(data);
-		byte[] bytesSAR = restTemplate.exchange(reportCertificateURL, HttpMethod.POST,
-				new HttpEntity<>(reportParams,httpHeaders), byte[].class).getBody();
+		byte[] bytesSAR = webClient.post().uri(reportCertificateURL).headers(h -> h.setBearerAuth(accessToken)).body(Mono.just(reportParams), GenerateReport.class).retrieve().bodyToMono(byte[].class).block();
 		byte[] encoded = Base64.encodeBase64(bytesSAR);
 	    return new String(encoded,StandardCharsets.US_ASCII);
 
@@ -304,7 +286,7 @@ public class GraduationService {
 
 
 
-	private ReportData prepareReportData(GraduationData graduationDataStatus, HttpHeaders httpHeaders,List<CodeDTO> specialProgram) {
+	private ReportData prepareReportData(GraduationData graduationDataStatus, String accessToken,List<CodeDTO> specialProgram) {
 		GradSearchStudent gradStudent = graduationDataStatus.getGradStudent();
 		GradAlgorithmGraduationStatus gradAlgorithm = graduationDataStatus.getGradStatus();
 		ReportData data = new ReportData();
@@ -319,8 +301,7 @@ public class GraduationService {
 		data.setSchool(graduationDataStatus.getSchool());
 		GraduationMessages graduationMessages = new GraduationMessages();
 		if(gradAlgorithm.getProgram() != null) {
-			GradProgram gradProgram = restTemplate.exchange(String.format(getGradProgramName,gradAlgorithm.getProgram()), HttpMethod.GET,
-    				new HttpEntity<>(httpHeaders), GradProgram.class).getBody();
+			GradProgram gradProgram = webClient.get().uri(String.format(getGradProgramName,gradAlgorithm.getProgram())).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(GradProgram.class).block();
 			graduationMessages.setGradProgram(gradProgram.getProgramName());
 			data.getDemographics().setProgram(gradProgram.getProgramCode());
 		}
