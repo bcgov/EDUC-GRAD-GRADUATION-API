a2ffdec671633be8a282e22e063b4da832aff608
Jinil Sung <infstar@gmail.com>
2022-09-19 16:39:38 -0700

    Update TokenUtils.java
    updated the comment in the log.

--- api/src/main/java/ca/bc/gov/educ/api/graduation/util/TokenUtils.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/util/TokenUtils.java
@@ -33,7 +33,7 @@ public class TokenUtils {
 
         logger.info("=========> Check Duration: {} sec <===========", diff);
         if (diff > 180) { // if the previous step took more than 3 minutes, treat it as a long process, and get the new access token
-            logger.info("=========> Getting the new Access Token after 4 minutes <===========");
+            logger.info("=========> Getting the new Access Token after 3 minutes <===========");
             ResponseObj responseObj = getTokenResponseObject();
             if (responseObj != null) {
                 return Pair.of(responseObj.getAccess_token(), currentTime);
