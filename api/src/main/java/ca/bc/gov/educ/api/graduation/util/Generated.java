c47d1b739ff85e7b25cf042eb5b5a33638435c8c
alexryba <alex.rybakov@gov.bc.ca>
2022-09-28 10:42:48 -0600

    GRAD2-1507 The Graduation School in the grad message box on Transcripts is incorrect

--- api/src/main/java/ca/bc/gov/educ/api/graduation/util/Generated.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/util/Generated.java
@@ -6,10 +6,10 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 @Documented
-@Retention(RUNTIME)
+@Retention(CLASS)
 @Target({TYPE, METHOD})
 public @interface Generated {
 }
