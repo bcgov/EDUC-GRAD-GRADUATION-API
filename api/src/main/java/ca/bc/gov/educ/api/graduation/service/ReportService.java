e6c9950183a372279c83b66d2e51177672000058
alexryba <alex.rybakov@gov.bc.ca>
2022-11-30 14:14:02 -0700

    GRAD2-1716 Student Demographic Json change for STS

--- api/src/main/java/ca/bc/gov/educ/api/graduation/service/ReportService.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/service/ReportService.java
@@ -501,7 +501,15 @@ public class ReportService {
                 data.setGraduationDate(EducGraduationApiUtils.formatIssueDateForReportJasper(EducGraduationApiUtils.parsingNFormating(graduationDataStatus.getGradStatus().getProgramCompletionDate())));
             }
         }
-
+        List<GradAlgorithmOptionalStudentProgram> optionalGradStatus = graduationDataStatus.getOptionalGradStatus();
+        optionalGradStatus.removeIf(p -> "DD".equalsIgnoreCase(p.getOptionalProgramCode()) || "FR".equalsIgnoreCase(p.getOptionalProgramCode()));
+        for(GradAlgorithmOptionalStudentProgram op: optionalGradStatus) {
+            if("FI".equalsIgnoreCase(op.getOptionalProgramCode())) {
+                data.getProgramCodes().add("FIP");
+            } else {
+                data.getProgramCodes().add(op.getOptionalProgramCode());
+            }
+        }
         return data;
     }
 
