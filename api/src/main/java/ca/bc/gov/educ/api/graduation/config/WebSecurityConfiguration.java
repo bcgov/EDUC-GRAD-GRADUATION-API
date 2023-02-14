03abeb56cb9b68576c01729d1ca4a3349414726f
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2023-02-14 15:53:47 -0800

    Removed unused imports.

--- api/src/main/java/ca/bc/gov/educ/api/graduation/config/WebSecurityConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/config/WebSecurityConfiguration.java
@@ -2,14 +2,12 @@ package ca.bc.gov.educ.api.graduation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
