54b39af46fbc6cb75992650320a5c7c247f51aef
alexryba <alex.rybakov@gov.bc.ca>
2023-04-24 11:06:50 -0600

    Fix timezone

--- api/src/main/java/ca/bc/gov/educ/api/graduation/config/EducGraduationApplicationConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/config/EducGraduationApplicationConfig.java
@@ -5,7 +5,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.TimeZone;
 
 @Configuration
 public class EducGraduationApplicationConfig {
@@ -25,6 +28,19 @@ public class EducGraduationApplicationConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+                // Set timezone for JSON serialization as system timezone
+                .timeZone(TimeZone.getDefault())
+                .build();
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
     }
 
 }
