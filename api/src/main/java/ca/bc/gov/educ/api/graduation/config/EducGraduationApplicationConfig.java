265b6e24f099fdab88bbdfe3ac052f9ce1e7bf85
alexryba <alex.rybakov@gov.bc.ca>
2023-04-26 14:40:27 -0600

    Fix adult start date format

--- api/src/main/java/ca/bc/gov/educ/api/graduation/config/EducGraduationApplicationConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/config/EducGraduationApplicationConfig.java
@@ -1,17 +1,11 @@
 package ca.bc.gov.educ.api.graduation.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.format.DateTimeFormatter;
-
-import static ca.bc.gov.educ.api.graduation.util.EducGraduationApiConstants.DATETIME_FORMAT;
 
 @Configuration
 public class EducGraduationApplicationConfig {
@@ -31,14 +25,6 @@ public class EducGraduationApplicationConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
-        LocalDateTimeSerializer localDateTimeSerializer = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
-        return jacksonObjectMapperBuilder ->
-                jacksonObjectMapperBuilder
-                        .serializers(localDateTimeSerializer);
     }
 
 }
