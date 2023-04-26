584d545428ca8cc57053ff5cd3e08271d910c5fb
alexryba <alex.rybakov@gov.bc.ca>
2023-04-26 14:53:16 -0600

    Fix adult start date format

--- api/src/main/java/ca/bc/gov/educ/api/graduation/model/dto/GradAlgorithmGraduationStudentRecord.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/model/dto/GradAlgorithmGraduationStudentRecord.java
@@ -1,10 +1,13 @@
 package ca.bc.gov.educ.api.graduation.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
 import java.util.UUID;
+
+import static ca.bc.gov.educ.api.graduation.util.EducGraduationApiConstants.DEFAULT_DATE_FORMAT;
 
 @Data
 @Component
@@ -26,5 +29,6 @@ public class GradAlgorithmGraduationStudentRecord {
     private String schoolAtGrad;
     private String consumerEducationRequirementMet;
     private String schoolAtGradName;
+    @JsonFormat(pattern=DEFAULT_DATE_FORMAT)
     private Date adultStartDate;
 }
