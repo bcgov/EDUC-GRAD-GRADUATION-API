e4456db4f2ec74beacbba88fa05841ee9aefd3fe
arybakov <alex.rybakov@cgi.com>
2023-06-08 12:25:39 -0600

    Fix tests coverage

--- api/src/test/java/ca/bc/gov/educ/api/graduation/service/ReportServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/graduation/service/ReportServiceTest.java
@@ -137,6 +137,22 @@ public class ReportServiceTest {
 	}
 
 	@Test
+	public void testGetStudentsForSchoolReport() {
+		List<ReportGradStudentData> gradStudentDataList = createStudentSchoolYearEndData("json/studentSchoolYearEndResponse.json");
+		ParameterizedTypeReference<List<ReportGradStudentData>> reportGradStudentDataType = new ParameterizedTypeReference<>() {
+		};
+
+		when(this.webClient.get()).thenReturn(this.requestHeadersUriMock);
+		when(this.requestHeadersUriMock.uri(constants.getSchoolStudents())).thenReturn(this.requestHeadersMock);
+		when(this.requestHeadersMock.headers(any(Consumer.class))).thenReturn(this.requestHeadersMock);
+		when(this.requestHeadersMock.retrieve()).thenReturn(this.responseMock);
+		when(this.responseMock.bodyToMono(reportGradStudentDataType)).thenReturn(Mono.just(gradStudentDataList));
+
+		var result = reportService.getStudentsForSchoolReport("accessToken");
+		assertNotNull(result);
+	}
+
+	@Test
 	public void testSaveStudentCertificateReport() {
 		UUID studentID = new UUID(1, 1);
 		ExceptionMessage exception = new ExceptionMessage();
