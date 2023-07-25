9b777636d912dba2275c7d57bc69091f0bc58cbb
arybakov <alex.rybakov@cgi.com>
2023-07-25 10:11:43 -0600

    Fix date format

--- api/src/main/java/ca/bc/gov/educ/api/graduation/exception/EntityNotFoundException.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/exception/EntityNotFoundException.java
@@ -1,8 +1,11 @@
 package ca.bc.gov.educ.api.graduation.exception;
 
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -26,7 +29,7 @@ public class EntityNotFoundException extends RuntimeException {
     private static <K, V> Map<K, V> toMap(
             Class<K> keyType, Class<V> valueType, Object... entries) {
         if (entries.length % 2 == 1)
-            throw new IllegalArgumentException("Invalid entries");
+            throw new IllegalArgumentException(Arrays.stream(entries).map(Object::toString).collect(Collectors.joining(",")));
         return IntStream.range(0, entries.length / 2).map(i -> i * 2)
                 .collect(HashMap::new,
                         (m, i) -> m.put(keyType.cast(entries[i]), valueType.cast(entries[i + 1])),
