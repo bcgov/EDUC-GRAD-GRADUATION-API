e83dd6a937032881fdc1182082fe6f6d177f70ce
arybakov <alex.rybakov@cgi.com>
2023-07-26 18:49:11 -0600

    Fix Date parsing error

--- api/src/main/java/ca/bc/gov/educ/api/graduation/util/GradLocalDateSerializer.java
+++ api/src/main/java/ca/bc/gov/educ/api/graduation/util/GradLocalDateSerializer.java
@@ -3,6 +3,8 @@ package ca.bc.gov.educ.api.graduation.util;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -10,11 +12,16 @@ import java.time.format.DateTimeFormatter;
 
 public class GradLocalDateSerializer extends JsonSerializer<LocalDate> {
 
+    private static final Logger logger = LoggerFactory.getLogger(GradLocalDateSerializer.class);
+
     @Override
     public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String fieldName = jsonGenerator.getOutputContext().getCurrentName();
         DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
         if(localDate != null) {
-            jsonGenerator.writeString(localDate.format(formatter));
+            String value = localDate.format(formatter);
+            logger.debug("Serialize LocalDate of {} to value {}", fieldName, value);
+            jsonGenerator.writeString(value);
         }
     }
 }
