acf645f44ff474c2204b0f475944187528cba648
mightycox <j.acox@hotmail.com>
2025-01-30 23:40:42 -0800

    GRAD2-3176 - adds tests

--- api/src/test/java/ca/bc/gov/educ/api/graduation/service/DistrictReportServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/graduation/service/DistrictReportServiceTest.java
@@ -12,6 +12,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,6 +70,25 @@ class DistrictReportServiceTest extends BaseServiceTest {
   }
 
   @Test
+  void createAndStoreDistrictReportMonth() {
+    List<ReportGradStudentData> studentData = createStudentSchoolYearEndData();
+    District district = createDistrict();
+    when(reportService.getStudentsForSchoolReport()).thenReturn(studentData);
+    studentData.forEach(student -> {
+          UUID schoolId = student.getSchoolAtGradId() == null ? UUID.fromString(student.getSchoolOfRecordId()) : UUID.fromString(student.getSchoolAtGradId());
+          School school = createSchool(UUID.fromString(district.getDistrictId()), schoolId, student.getMincode());
+          when(schoolService.getSchoolById(schoolId)).thenReturn(school);
+        }
+    );
+    when(districtService.getDistrictDetails(any(UUID.class))).thenReturn(district);
+    when(restService.post(any(), any(), any())).thenReturn(new byte[0]);
+
+    int result = districtReportService.createAndStoreDistrictReportMonth();
+
+    assertEquals(1, result);
+  }
+
+  @Test
   void createAndStoreDistrictNonGradYearEndReport() {
     List<ReportGradStudentData> studentData = createStudentSchoolYearEndData();
     District district = createDistrict();
@@ -96,5 +118,71 @@ class DistrictReportServiceTest extends BaseServiceTest {
 
     assertEquals(1, result);
 
+  }
+
+  @Test
+  void getDistrictYearEndReports() throws IOException {
+    List<ReportGradStudentData> studentData = createStudentSchoolYearEndData();
+    District district = createDistrict();
+    when(reportService.getStudentsForSchoolYearEndReport()).thenReturn(studentData);
+    studentData.forEach(student -> {
+          UUID schoolId = student.getSchoolAtGradId() == null ? UUID.fromString(student.getSchoolOfRecordId()) : UUID.fromString(student.getSchoolAtGradId());
+          School school = createSchool(UUID.fromString(district.getDistrictId()), schoolId, student.getMincode());
+          when(schoolService.getSchoolById(schoolId)).thenReturn(school);
+        }
+    );
+    when(districtService.getDistrictDetails(any(UUID.class))).thenReturn(district);
+    byte[] mockPdfBytes = Files.readAllBytes(Paths.get("src/test/resources/data/sample.pdf"));
+    when(restService.post(any(), any(), any())).thenReturn(mockPdfBytes);
+
+    byte[] result = districtReportService.getDistrictYearEndReports();
+
+    assertNotNull(result);
+    verify(reportService, times(1)).getStudentsForSchoolYearEndReport();
+    verify(restService, atLeastOnce()).post(any(), any(), any());
+  }
+
+  @Test
+  void getDistrictYearEndNonGradReports() throws IOException {
+    List<ReportGradStudentData> studentData = createStudentSchoolYearEndData();
+    District district = createDistrict();
+    when(reportService.getStudentsForSchoolNonGradYearEndReport()).thenReturn(studentData);
+    studentData.forEach(student -> {
+          UUID schoolId = student.getSchoolAtGradId() == null ? UUID.fromString(student.getSchoolOfRecordId()) : UUID.fromString(student.getSchoolAtGradId());
+          School school = createSchool(UUID.fromString(district.getDistrictId()), schoolId, student.getMincode());
+          when(schoolService.getSchoolById(schoolId)).thenReturn(school);
+        }
+    );
+    when(districtService.getDistrictDetails(any(UUID.class))).thenReturn(district);
+    byte[] mockPdfBytes = Files.readAllBytes(Paths.get("src/test/resources/data/sample.pdf"));
+    when(restService.post(any(), any(), any())).thenReturn(mockPdfBytes);
+
+    byte[] result = districtReportService.getDistrictYearEndNonGradReports();
+
+    assertNotNull(result);
+    verify(reportService, times(1)).getStudentsForSchoolNonGradYearEndReport();
+    verify(restService, atLeastOnce()).post(any(), any(), any());
+  }
+
+  @Test
+  void getDistrictReports() throws IOException {
+    List<ReportGradStudentData> studentData = createStudentSchoolYearEndData();
+    District district = createDistrict();
+    when(reportService.getStudentsForSchoolReport()).thenReturn(studentData);
+    studentData.forEach(student -> {
+          UUID schoolId = student.getSchoolAtGradId() == null ? UUID.fromString(student.getSchoolOfRecordId()) : UUID.fromString(student.getSchoolAtGradId());
+          School school = createSchool(UUID.fromString(district.getDistrictId()), schoolId, student.getMincode());
+          when(schoolService.getSchoolById(schoolId)).thenReturn(school);
+        }
+    );
+    when(districtService.getDistrictDetails(any(UUID.class))).thenReturn(district);
+    byte[] mockPdfBytes = Files.readAllBytes(Paths.get("src/test/resources/data/sample.pdf"));
+    when(restService.post(any(), any(), any())).thenReturn(mockPdfBytes);
+
+    byte[] result = districtReportService.getDistrictReports();
+
+    assertNotNull(result);
+    verify(reportService, times(1)).getStudentsForSchoolReport();
+    verify(restService, atLeastOnce()).post(any(), any(), any());
   }
 }
